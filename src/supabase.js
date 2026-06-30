5ab9fc9a13179307cec25f0e17d10045d085dded
🌸𝐌𝐢𝐤𝐮 ̶𝐂𝐡𝐚𝐧 ̶🩵✨ <apellidos05.nombre05@gmail.com>
2026-06-29 23:25:00 -0600

    Create supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
-const SUPABASE_URL = "";
+const SUPABASE_URL = "https://myxhwpnkmrznrrsqwysu.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_9FDvd5tbSkrZJfd-qZeNBQ_49AqBWd_";
 
 export const supabase = createClient(
